6dae73f5321e7bab006d81e767648d1a3bf77b75
vishalbodkhe531 <vishalbodkhe531@gmail.com>
2025-03-09 19:31:09 +0530

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+const page = () => {
+  return (
+    <>
+      <Button className="bg-red-500">hello</Button>
+    </>
+  );
+};
+
+export default page;
